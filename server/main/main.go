371b66bbb9cf75ff113d2a672d75aa96bce810cd
Jamal O'Garro <jamal.ogarro[at]gmail.com>
2018-01-04 18:03:01 -0500

    create sample endpoints getting users and a single user

--- server/main/main.go
+++ server/main/main.go
@@ -6,15 +6,36 @@ import(
   "github.com/labstack/echo"
 )
 
+func rootHandler (c echo.Context) error {
+  return c.String(http.StatusOK, "Server started")
+}
+
+func userHandler (c echo.Context) error {
+  userId := c.Param("userId")
+  fmt.Println(userId)
+  
+  return c.String(http.StatusOK, fmt.Sprintf("User Id: %s", userId))
+}
+
+func usersHandler (c echo.Context) error {
+  return c.String(http.StatusOK, "user handler")
+}
+
+func createUser(c echo.Context) error {
+  return true;
+}
+
 func main() {
   fmt.Println("Server initializing")
   
   e := echo.New()
   
   // "/" represents the root URL.
-  e.GET("/", func (c echo.Context) error {
-    return c.String(http.StatusOK, "Server started")
-  })
+  e.GET("/", rootHandler)
+    
+  e.GET("/users", usersHandler)
+  
+  e.GET("/users/:userId", userHandler)
   
   // e.Start starts the server.
   e.Start(":8000")
