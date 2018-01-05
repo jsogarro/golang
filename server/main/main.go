6f2837dedfbb751e017c73db50decaaa40187a22
Jamal O'Garro <jamal.ogarro[at]gmail.com>
2018-01-04 20:29:29 -0500

    add logger middleware to routes

--- server/main/main.go
+++ server/main/main.go
@@ -3,6 +3,7 @@ package main
 import(
   "fmt"
   "github.com/labstack/echo"
+  "github.com/labstack/echo/middleware"
 )
 
 func main() {
@@ -11,13 +12,13 @@ func main() {
   e := echo.New()
   
   // "/" represents the root URL.
-  e.GET("/", rootHandler)
+  e.GET("/", rootHandler, middleware.Logger())
     
-  e.GET("/users", getUsers)
+  e.GET("/users", getUsers, middleware.Logger())
   
-  e.GET("/users/:userId", getUser)
+  e.GET("/users/:userId", getUser, middleware.Logger())
   
-  e.POST("/users", createUser)
+  e.POST("/users", createUser, middleware.Logger())
   
   // e.POST("/users/:userId", editUser)
   // 
